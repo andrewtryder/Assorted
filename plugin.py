99d5fd7a165e622d904346c98526e1db97fc4dda
spline <reticulatingspline@github.com>
2013-02-12 11:03:06 -0500

    Add in a piglatin function.

--- plugin.py
+++ plugin.py
@@ -108,6 +108,42 @@ class Assorted(callbacks.Plugin):
 
     b64encode = wrap(b64encode, [('somethingWithoutSpaces')])
 
+    def pigword(self, word):
+        shouldCAP = (word[:1] == word[:1].upper())
+        word = word.lower()
+
+        letters = "qwertyuiopasdfghjklzxcvbnm"
+        i = len(word) - 1
+        while i >= 0 and letters.find(word[i]) == -1:
+            i = i - 1
+        if i == -1:
+            return word
+        punctuation = word[i+1:]
+        word = word[:i+1]
+
+        vowels = "aeiou"
+        if vowels.find(word[0]) >= 0:
+            word = word + "yay" + punctuation
+        else:
+            word = word[1:] + word[0] + "ay" + punctuation
+
+        if shouldCAP:
+            return word[:1].upper() + word[1:]
+        else:
+            return word
+
+    def piglatin(self, irc, msg, args, optinput):
+        """<text>
+        Convert text from English to Pig Latin.
+        """
+
+        l = optinput.split(" ")
+        for i in range(len(l)):
+            l[i] = self.pigword(l[i])
+
+        irc.reply(" ".join(l))
+
+    piglatin = wrap(piglatin, [('text')])
 
     def _frinkcleanup(self, text):
         text = text.replace(' in ', ' -> ')
