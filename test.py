1b812b2ea4d20b6259609e7655669293bc95d563
spline <reticulatingspline@github.com>
2014-10-12 07:27:09 -0400

    Tests..

--- test.py
+++ test.py
@@ -9,6 +9,17 @@ from supybot.test import *
 
 class AssortedTestCase(PluginTestCase):
     plugins = ('Assorted',)
+    
+    def testAssorted(self):
+        # advice, automeme, b64decode, b64encode, bash, bitcoin, bofh, callook
+        # catfacts, catpix, chucknorris, debt, developerexcuses, dogecoin, fml, frink, fuckingdinner,
+        # geoip, hackernews, hex2ip, hipster, kernel, litecoin, macvendor, megamillions, mortgage,
+        # mydrunktexts, nerdman, pick, piglatin, powerball, randomfacts, slur, and woot
+        self.assertNotError('advice')
+        self.assertNotError('automeme')
+        self.assertResponse('b64decode aGVsbG8=', 'hello')
+        self.assertResponse('b64encode hello', 'aGVsbG8=')
+        self.assertNotError('bash')
 
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
